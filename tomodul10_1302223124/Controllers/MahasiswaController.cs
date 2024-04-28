1ffc84cc01dc2ecfee1e1ab567847e80be347a6d
Deavanzack <daffafadillah564@gmail.com>
2024-04-29 00:23:44 +0700

    menambahkan API Mahasiswa

---
+++ tomodul10_1302223124/Controllers/MahasiswaController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tpmodul10_1302223124;
+
+namespace tomodul10_1302223124.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        public static List<Mahasiswa> mhs = new List<Mahasiswa>
+        {
+            new Mahasiswa("Muhammad Daffa Fadillah", "1302223124"),
+            new Mahasiswa("Muhammad Fauzan Majid", "1302220144"),
+            new Mahasiswa("Arinza Aurelvia", "1302220022"),
+            new Mahasiswa("⁠Muhammad Hashfi Hadyan", "1302220079"),
+            new Mahasiswa("Soraya Haidar Salma", "1302223006"),
+            new Mahasiswa("Maulana Farras", "103022390001")
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return mhs;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa newvalue)
+        {
+            mhs.Add(newvalue);
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return mhs[id];
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            mhs.RemoveAt(id);
+        }
+    }
+
+}
